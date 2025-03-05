ce1882a472d185194247f73d243dcda8eb8add26
Andrew <heyitsat12@gmail.com>
2025-03-05 18:30:18 -0500

    add todo and switch HL for memhl

--- src/Opcodes.java
+++ src/Opcodes.java
@@ -5,7 +5,7 @@ public class Opcodes {
         // TODO: figure out how to handle prefixed/unprefixed opcodes
         // nvm its simple; switch to prefixed when CB instruction is executed
         opcodesArray = new Instruction[0x100]; // 0xFF represents 255 in binary, but array needs to hold 256 values
-        opcodesArray[0x02] = new Instruction(Operation.LD, Operand.BC, Operand.A, 1);
+        opcodesArray[0x02] = new Instruction(Operation.LD, Operand.MemBC, Operand.A, 1);
         opcodesArray[0x09] = new Instruction(Operation.ADD16, Operand.HL, Operand.BC, 1);
         opcodesArray[0x19] = new Instruction(Operation.ADD16, Operand.HL, Operand.DE, 1);
         opcodesArray[0x29] = new Instruction(Operation.ADD16, Operand.HL, Operand.HL, 1);
@@ -16,15 +16,16 @@ public class Opcodes {
         opcodesArray[0x83] = new Instruction(Operation.ADD, Operand.E, 1);
         opcodesArray[0x84] = new Instruction(Operation.ADD, Operand.H, 1);
         opcodesArray[0x85] = new Instruction(Operation.ADD, Operand.L, 1);
-        opcodesArray[0x86] = new Instruction(Operation.ADD, Operand.HL, 1);
+        opcodesArray[0x86] = new Instruction(Operation.ADD, Operand.MemHL, 1);
         opcodesArray[0x87] = new Instruction(Operation.ADD, Operand.A, 1); 
+        // TODO: implement subtraction properly
         opcodesArray[0x90] = new Instruction(Operation.SUB, Operand.B, 1); 
         opcodesArray[0x91] = new Instruction(Operation.SUB, Operand.C, 1); 
         opcodesArray[0x92] = new Instruction(Operation.SUB, Operand.D, 1); 
         opcodesArray[0x93] = new Instruction(Operation.SUB, Operand.E, 1); 
         opcodesArray[0x94] = new Instruction(Operation.SUB, Operand.H, 1); 
         opcodesArray[0x95] = new Instruction(Operation.SUB, Operand.L, 1); 
-        opcodesArray[0x96] = new Instruction(Operation.SUB, Operand.HL, 1); 
+        opcodesArray[0x96] = new Instruction(Operation.SUB, Operand.MemHL, 1); 
         opcodesArray[0x97] = new Instruction(Operation.SUB, Operand.A, 1); 
         opcodesArray[0xC6] = new Instruction(Operation.ADD, Operand.n8, 2);  
         opcodesArray[0xE8] = new Instruction(Operation.ADD16, Operand.SP, Operand.e8, 2);  
