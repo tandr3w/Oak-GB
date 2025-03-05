edfd980eb9716d79920bc46606d439ef71ce0a52
Andrew <heyitsat12@gmail.com>
2025-03-05 10:51:15 -0500

    create unit test file

---
+++ src/Unit_Tests.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Unit_Tests {
+    public CPU cpu;
+    public Opcodes opcodes;
+
+    public Unit_Tests(){
+        cpu = new CPU();
+        opcodes = new Opcodes();
+    }
+
+    public int run(){
+        // Test add operation
+        return 1;
+    }
+}
