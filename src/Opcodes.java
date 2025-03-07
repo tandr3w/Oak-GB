49ab4d6c185b437e6ccb0c520ebc0b71138d3159
Andrew <heyitsat12@gmail.com>
2025-03-06 19:47:17 -0500

    remove the not implemented thing again

--- src/Opcodes.java
+++ src/Opcodes.java
@@ -269,7 +269,7 @@ public class Opcodes {
 
     public Instruction byteToInstruction(int readByte) {
         if (opcodesArray[readByte] == null){
-            System.out.println("Opcode " + Util.hexByte(readByte) + " is not implemented yet.");
+            // System.out.println("Opcode " + Util.hexByte(readByte) + " is not implemented yet.");
             return null;
         }
         return opcodesArray[readByte];
