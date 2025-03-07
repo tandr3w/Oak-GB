1309687ccade981630479166189eaee52550705b
ayleri <ailenmeng10@gmail.com>
2025-03-07 08:50:47 -0500

    Correctly updated zero flag for rotate methods

--- src/CPU.java
+++ src/CPU.java
@@ -739,7 +739,7 @@ public class CPU {
         int firstCircularBit = (registers.readValFromEnum(target) & 0b10000000) >> 7;
         int shifted = (registers.a << 1) & 0xFF;
         registers.setValToEnum(target, shifted | firstCircularBit);
-        registers.set_f_zero(false);
+        registers.set_f_zero(registers.readValFromEnum(target) == 0);
         registers.set_f_halfcarry(false);
         registers.set_f_subtract(false);
         registers.set_f_carry(firstCircularBit == 1);
@@ -749,7 +749,7 @@ public class CPU {
         int firstBit = (registers.readValFromEnum(target) & 0b10000000) >> 7;
         int shifted = (registers.a << 1) & 0xFF;
         registers.setValToEnum(target, shifted | registers.get_f_carry());
-        registers.set_f_zero(false);
+        registers.set_f_zero(registers.readValFromEnum(target) == 0);
         registers.set_f_halfcarry(false);
         registers.set_f_subtract(false);
         registers.set_f_carry(firstBit == 1);
@@ -759,7 +759,7 @@ public class CPU {
         int firstCircularBit = (registers.readValFromEnum(target) & 0b00000001) << 7;
         int shifted = (registers.a >> 1);
         registers.setValToEnum(target, shifted | firstCircularBit);
-        registers.set_f_zero(false);
+        registers.set_f_zero(registers.readValFromEnum(target) == 0);
         registers.set_f_halfcarry(false);
         registers.set_f_subtract(false);
         registers.set_f_carry(firstCircularBit == 1);
@@ -769,7 +769,7 @@ public class CPU {
         int firstBit = (registers.readValFromEnum(target) & 0b00000001) << 7;
         int shifted = (registers.a >> 1);
         registers.setValToEnum(target, shifted | registers.get_f_carry());
-        registers.set_f_zero(false);
+        registers.set_f_zero(registers.readValFromEnum(target) == 0);
         registers.set_f_halfcarry(false);
         registers.set_f_subtract(false);
         registers.set_f_carry(firstBit == 1);
