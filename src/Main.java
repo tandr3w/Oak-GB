89dd0c4fe46c77db4aec02e98c80288ffe85820c
Andrew <heyitsat12@gmail.com>
2025-03-19 09:07:04 -0400

    play pokemon

--- src/Main.java
+++ src/Main.java
@@ -68,7 +68,7 @@ public class Main extends JFrame implements KeyListener {
         
         // https://github.com/mattcurrie/dmg-acid2
         // memory.loadROM("ROMs/dmg-acid2.gb"); // graphics testing ROM
-        memory.loadROM("ROMs/Zelda.gb");
+        memory.loadROM("ROMs/PokemonReal.gb");
         memory.loadSave();
         // memory.loadROM("ROMs/mooneye-wario-suite/acceptance/bits/unused_hwio-GS.gb"); // Failed
         // memory.loadROM("ROMs/blargg-test-roms/cpu_instrs/cpu_instrs.gb"); // Passed
