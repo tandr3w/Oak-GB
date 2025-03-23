0a0eba14dc93952a59e5338332b7b478eef09087
ayleri <ailenmeng10@gmail.com>
2025-03-23 15:40:26 -0400

    update get frequency function

--- src/Memory.java
+++ src/Memory.java
@@ -217,7 +217,7 @@ public class Memory {
 
 
     public int getFrequencyC2(){
-        return (getMemory(NR23_address) << 8) | (getMemory(NR24_address));
+        return ((getMemory(NR24_address) & 0b00000111) << 8) | (getMemory(NR23_address));
     }
 
     public int getNR21(){
