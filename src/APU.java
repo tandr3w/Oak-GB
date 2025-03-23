74a80a03924442d9a7c85b6e905b633139aec988
Andrew <heyitsat12@gmail.com>
2025-03-23 15:20:24 -0400

    'fix' audio

--- src/APU.java
+++ src/APU.java
@@ -35,22 +35,17 @@ public class APU {
         class AudioThread extends Thread {
             @Override
             public void run(){
-                int numberOfTimesFullSinFuncPerSec = 441; //number of times in 1sec sin function repeats
-                int counter = 0;
                 while (true){
-                    float numberOfSamplesToRepresentFullSin= (float) frequency / numberOfTimesFullSinFuncPerSec;
-                    double angle = (counter) / (numberOfSamplesToRepresentFullSin/ 2.0) * Math.PI;  // /divide with 2 since sin goes 0PI to 2PI
-                    short a = (short) (Math.sin(angle) * 32767);  //32767 - max value for sample to take (-32767 to 32767)
-                    if (angle % (2.0*Math.PI) > Math.PI){
+                    int a = 0;
+                    if (amplitude == 1){
                         a = 32767/2;
                     }
-                    else {
+                    else if (amplitude == 0){
                         a = -32767/2;
                     }
                     buf[0] = (byte) (a & 0xFF); //write 8bits ________WWWWWWWW out of 16
                     buf[1] = (byte) (a >> 8); //write 8bits WWWWWWWW________ out of 16
                     sdl.write(buf, 0, 2);
-                    counter += 1;
                 }   
             }
         }
