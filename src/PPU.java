4c3bf227d04e32832eb1341553b53fed90a7a2bf
Andrew <heyitsat12@gmail.com>
2025-03-30 13:25:00 -0400

    revert previous change

--- src/PPU.java
+++ src/PPU.java
@@ -537,7 +537,9 @@ public class PPU extends JPanel {
     }
 
     public void updateGraphics(int cycles) {
-
+        updateLCDStatus();
+        prevLY = memory.getLY();
+        prevLYC = memory.getLYC();
         if (memory.getLCDEnable() == 0) {
             return;
         }
@@ -546,9 +548,7 @@ public class PPU extends JPanel {
         int LY = memory.getLY();
         
         if (remainingCycles <= 0) {
-            updateLCDStatus();
-            prevLY = memory.getLY();
-            prevLYC = memory.getLYC();
+
             if (LY < 144) {
                 drawScanline();
             }
