b79f293311ce01c993b7bceb4cf64cc932a4c803
ailen meng <ailenmeng.dev@gmail.com>
2025-09-28 11:22:54 -0400

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,19 @@
-javac -d bin Main.java src/*.java
-java -cp bin Main
+#!/bin/bash
+set -e
+
+SRC_DIR="src"   # .java
+OUT_DIR="out"   # .class
+LIB_DIR="lib"
+MAIN_CLASS="Main"
+
+mkdir -p "$OUT_DIR"
+
+CP="."
+if [ -d "$LIB_DIR" ]; then
+    for jar in "$LIB_DIR"/*.jar; do
+        CP="$CP:$jar"
+    done
+fi
+
+javac -cp "$CP" -d "$OUT_DIR" $(find "$SRC_DIR" -name "*.java")
+java -cp "$CP:$OUT_DIR" "$MAIN_CLASS"
